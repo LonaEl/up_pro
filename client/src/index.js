47959539eacf52f42aaad3104b29aadf07b166b4
Lona Sontsele <1867792@students.wits.ac.za>
2022-10-25 21:51:42 +0200

    Lona

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import { applyMiddleware, compose } from 'redux';
 import { configureStore } from '@reduxjs/toolkit'
 import thunk from 'redux-thunk';
-
 import { reducers } from './reducers';
 import App from './App';
 import './index.css';
